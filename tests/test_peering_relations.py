90f58eaa3715ec49dbf73ad2584dd8a775a4e0ad
Rogier Krieger <rkrieger@users.noreply.github.com>
2023-05-10 20:51:36 +0200

    Add INTERIX. (#313)
    * Add INTERIX.
    
    Involves following changes:
    * README.md update
    * Adding IXP IPv4/v6 ranges to testing script
    * Peering definition for IXP route servers
      (incl. note regarding non-existing AS-SET)
    
    Also included a small, unrelated whitespace fix (AS44901).

--- tests/test_peering_relations.py
+++ tests/test_peering_relations.py
@@ -59,6 +59,8 @@ connected_ixps = {
                 ipaddr.IPNetwork('2001:7f8:24::/64')],
     "locix": [ipaddr.IPNetwork('185.1.138.0/24'),
               ipaddr.IPNetwork('2a0c:b641:700::/64')],
+    "interix": [ ipaddr.IPNetwork('185.0.1.0/24'),
+                ipaddr.IPNetwork('2001:7f8:134::/64')],
     "private-eun": [ipaddr.IPNetwork('62.115.144.32/31'),
                     ipaddr.IPNetwork('2001:2000:3080:0EBC::/126')],
     "multihop-eun": [ipaddr.IPNetwork('4.68.4.43/32'),
